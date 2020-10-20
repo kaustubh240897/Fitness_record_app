8edb2a50195f923607c7a8ea17c6f64e0d860cee
Kaustubh Dwivedi <kaustubh.dwi@gmail.com>
2020-10-21 00:12:23 +0530

    change in yml file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ use App\t_Tour;
 use App\t_Steps;
 use App\Observers\t_TourObserver;
 use App\Observers\t_StepObserver;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,6 +30,6 @@ class AppServiceProvider extends ServiceProvider
         date_default_timezone_set('Asia/Tokyo');
         t_Tour::observe(t_TourObserver::class);
         t_Steps::observe(t_StepObserver::class);
-        Schema::defaultStringLength(191);
+
     }
 }
