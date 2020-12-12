5d28c86e88ee630f22d4611201e8992b163b37fe
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2020-12-13 02:24:05 +0530

    added new mycollection page

--- resources/views/mycollections.blade.php
+++ resources/views/mycollections.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class='container'>
+ 
 @if (session('successMsg'))
   <div class='alert alert-success' roles='alert'>
     {{ session('successMsg') }}
   </div>
 @endif
 
-
+<head>
  <style media="screen">
       .btn.blue {
           background-color: #ccece8 !important;
@@ -17,73 +17,124 @@
       .collectionsRow {
         cursor: pointer;
       }
-      .checked {
-        color: #0066ff;
-      }
+     
+
+      .button {
+		  background-color: #2b63c6;
+		  color: white;
+		  text-align: center;
+		  text-decoration: none;
+		  display: inline-block;
+		  cursor: pointer;
+		  border-radius: 16px;
+		  border: 1px solid white;
+		}
+		
+		body {
+		  
+		  overflow-x:hidden;
+		}
+	
+		div.c {
+		  text-align: right;
+		} 
+
+		.wrapper1{
+	  		height:13rem;
+	  		font-size: 15px;
+	  		background-color: #2b63c6;
+	  		
+	  	}
+	  	.center {
+			  display: block;
+			  margin-left: auto;
+			  margin-right: auto;
+			  margin-top: 4rem;
+
+			  position: relative;
+			  z-index: 1;
+
+			  
+			}
+		  .circle {
+			  
+			  color: white;
+			  text-decoration: none;
+			  display: inline-block;
+			  border-radius: 2px;
+			}
+
+			.circle .badge {
+			  position: absolute;
+			  color: black;
+			}
+			.checked {
+			  color: #ffcc00;
+			}
+
+			.border {
+				border: 1px solid grey;
+			}
+			
+
   </style>
 
+</head>
+<body style='background-color: white;'>
 
-<div class="container-fluid mt-2">
-      <div class="row justify-content-around">
-        <div class="col-xs-3">
-         <a href="{{ url('/mypage') }}" >  <button type="button" class="btn blue mr-1 mb-1" style="font-size:70%">マイページへ戻る</button> </a>
-        </div>
-        <div class="col-xs-3">
-         <a href="{{ url('/bytourcollection') }}" >  <button type="button" class="btn blue mr-1 mb-1" style="font-size:70%">ツアー別表示</button> </a>
-        </div>
-        <div class="col-xs-3">
-        <a href="{{ url('/reversemycollection') }}"> <button type="button" class="btn blue mr-1 mb-1" style="font-size:70%">取得の新しい順</button> </a>
-        </div>
-        <div class="col-xs-3">
-         <a href="{{ url('/mycollection') }}" > <button type="button" class="btn blue mr-1 mb-1" style="font-size:70%">取得の古い順</button> </a>
-        </div>
-      </div>
-    </div>
+<div class='container-fluid' >
+  <div class="row d-flex fixed-top" style="border-bottom: 1px solid white; z-index: 10; background-color: #2b63c6; position: fixed; top:0px;">
+   <div class=' p-2' style="color:white;"><div class="ml-4" style='color: white;'>私のコレクション</div> </div>
+   <div class='ml-auto p-2 c'>  <button class='button mr-4' style="color:white;">ツアーで並べ替え ↑↓ </button></div>
 
-<div class="table-responsive-sm mt-4">
-        <table class="table table-hover" id="collections_table">
-            <thead>
-              <tr>
-                <th scope="col">アイテムの種類</th>
-                <th scope="col">アイテムの名前</th>
-                <th scope="col">取得年月日</th>
-                <th scope="col">取得回数</th>
-              </tr>
-            </thead>
-  @if(! empty($get_t_collections))
-    <tbody>
+  </div> 
 
+    
+
+  <div class="row wrapper1 fixed-top" style='z-index: 0;'>	
+
+    <div class='col-12'><img class='center' src='/storage/img/star_mycol.png'> </div>
+   
+    
+  </div> 
+
+  @if(! empty($get_t_collections))   
+  <div class='row py-2' style='margin-top:15rem; z-index: 9; position: relative; background-color: white; padding-bottom: 0%;'>
   @foreach($get_t_collections as $get_t_collection)
-  @if($get_t_collection->m_collections->collection_category == 'tour')
-     <tr class="collectionsRow">
-      <td style="font-size:70%">{{ $get_t_collection->m_collections->collection_category }}  tour {{ $get_t_collection->m_collections->m__tours->id }}</td>
-      <td style="font-size:70%"><a href="{{ route('collectiondetails', $get_t_collection->m_collections->id) }}">{{ $get_t_collection->m_collections->collection_title }} </a></td>
-      <td style="font-size:70%"> {{ $get_t_collection->created_at }} </td>
-      <td style="font-size:70%"> {{ $counter[$loop->iteration-1] }}<span class="fa fa-star checked"></span></td>
-    </tr>
+    @if($get_t_collection->new_display_flag == 0)
+      <div class='col-4 circle'> 
+        <span class="badge badge-pill badge-warning">New</span>
+         <span> <img style='border: solid  #ffcc00'; src="{{ asset($get_t_collection->m_collections->path . $get_t_collection->m_collections->filename) }}" class="rounded-circle" alt="Cinque Terre" width="80" height="80"> </span> 
+      </div>
+    @else
+    <div class='col-4'> 
+        <img src="{{ asset($get_t_collection->m_collections->path . $get_t_collection->m_collections->filename) }}"  class=" rounded-circle" alt="Cinque Terre" width="80" height="80"> 
+      </div> 
+    @endif
+        <div class='col-8'> <img src='/storage/img/label-1.png'><br><a href="{{ route('collectiondetails', $get_t_collection->m_collections->id) }}">{{ $get_t_collection->m_collections->collection_title }} </a></div> 
+      <div class='col-4'> </div>
+      <div class='col-8 c checked'> @if($counter[$loop->iteration-1] >5)+@endif  @for($i=0; $i<$counter[$loop->iteration-1]; $i++) @if($i <=5)<img src='storage/img/star.png'>@endif @endfor
+        <font style='color:#cbaca3;'> {{ $get_t_collection->created_at->format('Y-m-d') }} </font>
+      </div>
+  
 
+                <br/>
+    @endforeach
 
-  @else
-     <tr class="collectionsRow">
-      <td style="font-size:70%"><a href="{{ route('checkpointdetails', $get_t_collection->m_collections->m__checkpoints->id) }}">{{ $get_t_collection->m_collections->collection_category }} tour {{ $get_t_collection->m_collections->m__checkpoints->m__tour_id }}</a></td>
-      <td style="font-size:70%"><a href="{{ route('collectiondetails', $get_t_collection->m_collections->id) }}">{{ $get_t_collection->m_collections->collection_title }}</a></td>
-      <td style="font-size:70%"> {{ $get_t_collection->created_at }} </td>
-      <td style="font-size:70%"> {{ $counter[$loop->iteration-1] }}<span class="fa fa-star checked"></td>
-    </tr>
-  @endif
+     
+    <div class='col-12 mt-1' style='background-color: #eef4f6; position: relative; z-index: 11; margin-top:16rem !important;'>
+      <img style='width: 100%;height: auto;' src='/storage/img/bg@3x.png'>
+    </div>
+    
+  </div>
 
-  @endforeach
-
-  </tbody>
-  @else
+  
+@else
     <h1> まだコレクションはありません！ </h1>
   @endif
-  </table>
-  </div>
-  <div class="toast" id="myToast">
-    <div class="toast-body">
-    あなたが私たちを訪ねてから久しぶりです。私たちはあなたのために特別な何かを持っています。
-    </div>
-</div>
+
+</body>
+
+ 
 
 @endsection
