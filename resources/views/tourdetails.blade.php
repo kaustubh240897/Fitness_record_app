f9da3c91d4abc95b180759c981eddf9087054c07
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2020-12-14 23:54:53 +0530

    added pop up in tourdetails

--- resources/views/tourdetails.blade.php
+++ resources/views/tourdetails.blade.php
@@ -394,7 +394,32 @@
                           <form action="{{ route('tourstore', $tours->id) }}" method="POST" >
                             {{ csrf_field() }}
                             {{ method_field('post') }}
-                            <button type='submit' class="button1  refBtn"><  このツアーを選択してください </button>
+                            <!-- Button trigger modal -->
+                            <button type='button' class="button1  refBtn" data-toggle="modal" data-target="#exampleModalCenter"><  このツアーを選択してください </button>
+                            
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                              <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                  </div>
+                                  <div class="modal-body">
+                                    sdhlhsdHDlAHDlAIHFASKLFHBAKJSFBASKJHFkHFKJ?fBLASBFLFBKASFASLFalsAF  sdhlhsdHDlAHDlAIHFASKLFHBAKJSFBASKJHFkHFKJ?fBLASBFLFBKASFASLFalsAF
+                                    sdhlhsdHDlAHDlAIHFASKLFHBAKJSFBASKJHFkHFKJ?fBLASBFLFBKASFASLFalsAF sdhlhsdHDlAHDlAIHFASKLFHBAKJSFBASKJHFkHFKJ?fBLASBFLFBKASFASLFalsAF
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Save changes</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            
                           </form>
                           </div>
                     </div>
