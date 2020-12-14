545301edf2797ea818f12d664596f7e1d2ee54a7
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2020-12-15 01:54:24 +0530

    added the index for my collection radiobutton

--- resources/views/mycollections.blade.php
+++ resources/views/mycollections.blade.php
@@ -139,6 +139,7 @@
     @endforeach
 
   </div>
+  </div>
     <div class="d-flex flex-row" style="position: absolute; bottom: 0; left: 0; background-color: #eef4f6;">
       <img class="w-100" src='/storage/img/bg@3x.png'>
     </div>
