7446b2939837b123342075ab100eb9b42760a581
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2020-12-20 15:06:55 +0530

    chnged number format in show profile details

--- resources/views/showprofiledetails.blade.php
+++ resources/views/showprofiledetails.blade.php
@@ -279,7 +279,7 @@
 								<div class='mt-4'> <img style='width:100%; height: auto;' src='/storage/img/error@3x.png'></div>
 								@else
 								<h6><div class='row' ><div class='col-7' style='color: #2b63c6;'> {{ $t_tour->m_tours->tour_title }} </div> <div class='col-4 c' style='color:grey;'>@foreach($t_tour->m_tours->checkpoints as $checkpoint) @if($checkpoint->checkpoint_category =='endpoint') {{ $checkpoint->distance }} km @endif @endforeach </div> </div></h6>
-								<div class='col-12 text-center'> <b class='color'>{{ $m_users->stride*$steps/100000 }} km/@foreach($t_tour->m_tours->checkpoints as $checkpoint) @if($checkpoint->checkpoint_category =='endpoint') {{ $checkpoint->distance }} km  @endif @endforeach</b></div>
+								<div class='col-12 text-center'> <b class='color'>{{ number_format($m_users->stride*$steps/100000, 2, '.',',') }} km /@foreach($t_tour->m_tours->checkpoints as $checkpoint) @if($checkpoint->checkpoint_category =='endpoint') {{ $checkpoint->distance }} km  @endif @endforeach</b></div>
 								@endif
 							@else
 								<div class='mt-4'> <img style='width:100%; height: auto;' src='/storage/img/error-1@3x.png'></div>
--- resources/views/thank.blade.php
+++ resources/views/thank.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    <h3 class='text-center'> 私たちのアプリをご利用いただきありがとうございます。 </h3>
+    <h3 class='text-center mt-5'> 私たちのアプリをご利用いただきありがとうございます。 </h3>
 @endsection
