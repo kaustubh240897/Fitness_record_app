d5f74849101263df6dd6608be12f7c2383dbbdbf
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2021-01-15 12:35:44 +0530

    aa

--- app/Http/Resources/t_StepsResource.php
+++ app/Http/Resources/t_StepsResource.php
@@ -19,6 +19,8 @@ class t_StepsResource extends JsonResource
             'step_actual_datetime'=>$this->step_actual_datetime,
             'step_calc_datetime'=> $this->step_calc_datetime,
             'steps'=> $this->steps,
+            'created_at'=> $this->created_at,
+            'updated_at' => $this->updated_at,
             
         ];
     }
