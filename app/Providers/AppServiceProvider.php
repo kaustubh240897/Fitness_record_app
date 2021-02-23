04e09c405b9a2775d69d1252e244c037da797a0e
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2021-02-23 14:50:34 +0530

    removed step observer from app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         //
         date_default_timezone_set('Asia/Tokyo');
         t_Tour::observe(t_TourObserver::class);
-        t_Steps::observe(t_StepObserver::class);
+        //t_Steps::observe(t_StepObserver::class);
 
     }
 }
