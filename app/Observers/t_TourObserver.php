db8c50b51711ed9d0bd6ee5a85ee1793608fff77
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2021-03-16 12:35:24 +0530

    Removed deleting of collection while changing of tour

--- app/Observers/t_TourObserver.php
+++ app/Observers/t_TourObserver.php
@@ -21,7 +21,7 @@ class t_TourObserver
        $mytime = Carbon::now();
        $m__users_id = m_Users::where('users_id', Auth::id())->first()->id;
 
-    //   $get_t_tour = t_Tour::where('m__users_id', $m__users_id)->where('status', 'Inprogress')->orderBy('created_at', 'DESC')->first();
+    //    $get_t_tour = t_Tour::where('m__users_id', $m__users_id)->where('status', 'Inprogress')->orderBy('created_at', 'DESC')->first();
     //    if($get_t_tour !=null){
     //         $get_created_date = $get_t_tour->created_at;
             
