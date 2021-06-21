731ca3123ce94cdc0b3313a8233740059d972cc2
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2021-06-21 12:23:03 +0530

    removed console.log in production

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,18 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+    if (mix.inProduction()) {
+        mix.options({
+            terser: {
+                terserOptions: {
+                    compress: {
+                       drop_console: true
+                    }
+                }
+            }
+        });
+    
+        mix.version();
+    }
